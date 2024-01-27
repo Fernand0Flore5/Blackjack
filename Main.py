6bc32f50af398dd8215241bf57e4c8a9489834c1
Luis Fernando Flores Gutierrez <volcanick_charge@hotmail.com>
2024-01-27 16:30:38 -0600

    actualizacion

--- Main.py
+++ Main.py
@@ -1,6 +1,11 @@
+import random
+
+
+
 class mazo:
     def __init__(self, cartas = ["A","J","Q","K",2,3,4,5,6,7,8,9,10] ):
         self.cartas = cartas
-
-ejemplo = mazo()
-print(ejemplo.cartas)
+    def picas(self):
+        
+#ejemplo = mazo()
+#print(ejemplo.cartas)
